edd501a7dcfb281ff158dd918ed1770c06d336aa
Oleh Kokhan <olehspidey@gmail.com>
2018-10-10 22:04:11 +0300

    implemented model state filter and added to all routes

---
+++ b2b/src/B2B/Filters/ActionFilters/ModelStateFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace B2B.Filters.ActionFilters
+{
+    public class ModelStateFilter : ActionFilterAttribute
+    {
+        public string Message { get; set; } = "Invalid model";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(new { Message, State = context.ModelState });
+            }
+        }
+    }
+}
