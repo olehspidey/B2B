0c33a241af320b7e528a9d06d784d45d054aaa6f
Oleh Kokhan <olehspidey@gmail.com>
2018-10-12 00:04:54 +0300

    Fixed project to TS (#11)
    * created react application
    
    * fixed project to TS

---
+++ b2b-web/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        B2B Project
+      </div>
+    );
+  }
+}
+
+export default App;
