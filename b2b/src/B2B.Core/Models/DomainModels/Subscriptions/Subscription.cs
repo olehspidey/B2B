99ca74a02f77963788ee724305aca4204ecbc0fe
Oleh Kokhan <olehspidey@gmail.com>
2018-10-16 23:18:35 +0300

    added user and user subscription domain model

---
+++ b2b/src/B2B.Core/Models/DomainModels/Subscriptions/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace B2B.Core.Models.DomainModels.Subscriptions
+{
+    public class Subscription : IEntity<int>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime? End { get; set; }
+
+        public SubscriptionType SubscriptionType { get; set; }
+
+        public User User { get; set; }
+    }
+}
