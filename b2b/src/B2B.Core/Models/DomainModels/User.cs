d35afdf4fd4970e69b56c2d949fff3b4756d6a0f
Oleh Kokhan <olehspidey@gmail.com>
2018-10-16 23:19:30 +0300

    added user and user subscription domain model (#15)

--- b2b/src/B2B.Core/Models/DomainModels/User.cs
+++ b2b/src/B2B.Core/Models/DomainModels/User.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using B2B.Core.Models.DomainModels.Subscriptions;
+using Microsoft.AspNetCore.Identity;
 
 namespace B2B.Core.Models.DomainModels
 {
     public class User : IdentityUser, IEntity<string>
     {
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string LastName { get; set; }
+
+        public double Bill { get; set; }
+
+        public Subscription Subscription { get; set; }
     }
 }
