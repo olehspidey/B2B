c350494647344256e0d1fa557e6d243d765fadc0
Oleh Kokhan <olehspidey@gmail.com>
2018-10-20 18:00:46 +0300

    added creating user from froms (#19)

--- b2b/src/B2B.BLL/Services/IUserService.cs
+++ b2b/src/B2B.BLL/Services/IUserService.cs
@@ -1,7 +1,10 @@
-﻿namespace B2B.BLL.Services
+﻿using System.Threading.Tasks;
+using B2B.Core.Models.DomainModels;
+
+namespace B2B.BLL.Services
 {
     public interface IUserService
     {
-        
+        Task<User> CreateUserFromFormAsync(int formId);
     }
 }
