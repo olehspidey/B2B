b24ec24061754987e4b1f88a6d0936d043fdb208
Oleh Kokhan <olehspidey@gmail.com>
2018-10-25 19:14:23 +0300

    added base api

---
+++ b2b-web/src/Api/api.ts
@@ -0,0 +1,93 @@
+import api from 'axios';
+import { IBearerAuthorization } from './IBearerAuthorization';
+
+export const ACCESS_TOKE = 'token';
+
+const PROTOCOL = 'http';
+const HOST_NAME = 'localhost';
+const PORT = 5000;
+const PROD_PROTOCOL = 'https';
+const PROD_HOST_NAME = '';
+
+const getApiUrl = () => {
+    let apiUrl = `${PROTOCOL}://${HOST_NAME}:${PORT}`;
+
+    if (!process || !process.env || process.env.NODE_ENV !== 'development') {
+        apiUrl = `${PROD_PROTOCOL}://${PROD_HOST_NAME}`;
+    }
+
+    return apiUrl;
+}
+
+
+
+export const API_URL = getApiUrl();
+
+export const headerToken = (token: string | null): IBearerAuthorization => {
+    if (token === null)
+        throw new Error('Token can not be null');
+
+    return {
+        Authorization: `Bearer ${token}`
+    };
+}
+
+const getPrivateApi = () => {
+    if (localStorage.getItem(ACCESS_TOKE) === null) {
+        window.location.replace(`/login`);
+        return api;
+    }
+    else {
+        return api.create({
+            headers: headerToken(localStorage.getItem(ACCESS_TOKE))
+        });
+    }
+};
+
+export const apiGet = (url: string) => {
+    return getPrivateApi()
+        .get(`${API_URL}${url}`)
+        .then(resp => resp, ({ response }) => {
+            if (response.status === 401 || response.status === 403) {
+                window.location.replace('/login');
+                return response;
+            }
+            return Promise.reject(response);
+        });
+};
+
+export const apiPost = (url: string, body: any) => {
+    return getPrivateApi()
+        .post(`${API_URL}${url}`, body)
+        .then(resp => resp, ({ response }) => {
+            if (response.status === 401 || response.status === 403) {
+                window.location.replace('/login');
+                return response;
+            }
+            return Promise.reject(response);
+        });
+};
+
+export const apiPut = (url: string, body: any) => {
+    return getPrivateApi()
+        .put(`${API_URL}${url}`, body)
+        .then(resp => resp, ({ response }) => {
+            if (response.status === 401 || response.status === 403) {
+                window.location.replace('/login');
+                return response;
+            }
+            return Promise.reject(response);
+        });
+};
+
+export const apiDelete = (url: string) => {
+    return getPrivateApi()
+        .delete(`${API_URL}${url}`)
+        .then(resp => resp, ({ response }) => {
+            if (response.status === 401 || response.status === 403) {
+                window.location.replace('/login');
+                return response;
+            }
+            return Promise.reject(response);
+        });
+};
